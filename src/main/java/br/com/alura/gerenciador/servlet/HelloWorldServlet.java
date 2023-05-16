87d4b63a1c291ab5f56a9c9af59d0608c4fb98d2
miichaels <michael_araujo2012@hotmail.com>
2023-05-16 15:01:39 -0300

    Java Servlet - Programação Web Java

---
+++ src/main/java/br/com/alura/gerenciador/servlet/HelloWorldServlet.java
@@ -0,0 +1,30 @@
+ package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//oi
+@WebServlet(urlPatterns = "/oi")
+public class HelloWorldServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Hello world, parabéns você desenvolveu o primeiro Servlets.");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("O Servlet HelloWorld foi chamado ");
+		
+	}
+	
+}
